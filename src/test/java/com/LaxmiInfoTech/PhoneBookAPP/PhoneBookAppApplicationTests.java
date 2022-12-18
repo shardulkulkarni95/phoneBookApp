4e92ddeac38e7240a572945885bd58be2453cd5b
shardulkulkarni95 <you@example.com>
2022-12-18 19:48:20 +0530

    [200~PHN-2

---
+++ src/test/java/com/LaxmiInfoTech/PhoneBookAPP/PhoneBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LaxmiInfoTech.PhoneBookAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
